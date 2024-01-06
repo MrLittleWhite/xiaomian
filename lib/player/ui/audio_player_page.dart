e31963d6d96291ffeff2b3ab1cab2d36d762d307
YunzhiLiu <15013989079@163.com>
2024-01-06 15:48:35 +0800

    feat: 添加波浪装饰条

--- lib/player/ui/audio_player_page.dart
+++ lib/player/ui/audio_player_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:wave/config.dart';
+import 'package:wave/wave.dart';
 import 'package:xiaomian/assets_code/xm_color.dart';
 import 'package:xiaomian/assets_code/xm_font_family.dart';
 import 'package:xiaomian/gen/assets.gen.dart';
@@ -43,7 +46,7 @@ class _AudioPlayerPageState extends State<AudioPlayerPage> {
         body: Center(
           child: Column(
             children: [
-              Flexible(flex: 2, fit: FlexFit.tight, child: Column(
+              Flexible(flex: 6, fit: FlexFit.tight, child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ClipRRect(borderRadius: BorderRadius.circular(20), child: Image.network('https://cccimg.com/view.php/7ff3bd13cda0aaae9ad0de8d29411f56.jpeg', fit: BoxFit.cover, height: 170, width: 170,)),
@@ -53,40 +56,72 @@ class _AudioPlayerPageState extends State<AudioPlayerPage> {
                   Text("Pack name", style: XMTextStyle(color: Colors.white, fontSize: 34, fontWeight: FontWeight.w900),),
                 ]
               )),
-              Flexible(flex: 1, child: Center(
-                child: SizedBox(
-                  width: 72,
-                  height: 72,
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      StreamBuilder<bool>(
-                        stream: playerController.player.loadingStream,
-                        builder: (context, snapshot) {
-                          return playerController.player.isLoading ? LoadingAnimationWidget.threeArchedCircle(color: Colors.white, size: 56) : Container();
-                        },  
-                      ),
-                      StreamBuilder<bool>(
-                        stream: playerController.player.playingStream,
-                        builder: (context, snapshot) {
-                          return IconButton(
-                          onPressed: () {
-                            const model = AudioItem(id: "123", title: "东方红", albumTitle: "新中国", artist: "陕北人民", url: "https://cccimg.com/view.php/5d4086139dcd25bef2522c469512c83f.mp3", artwork: "https://cccimg.com/view.php/7ff3bd13cda0aaae9ad0de8d29411f56.jpeg");
-                            playerController.setPlayItem(model);
-                            playerController.handler?.playOrPause();
-                          },  
-                          icon: Icon(playerController.player.isPlaying ? Icons.pause : Icons.play_arrow, color: Colors.white, size: 36,),
-                          );
-                        },  
-                      ),
-                      ]
+              Flexible(flex: 4, child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  StreamBuilder<bool>(
+                            stream: playerController.player.playingStream,
+                            builder: (context, snapshot) {
+                    return _wave();
+                    },  
                   ),
-                ),
+                  Expanded(
+                    child: SizedBox(
+                      width: 72,
+                      height: 72,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          StreamBuilder<bool>(
+                            stream: playerController.player.loadingStream,
+                            builder: (context, snapshot) {
+                              return playerController.player.isLoading ? LoadingAnimationWidget.threeArchedCircle(color: Colors.white, size: 56) : Container();
+                            },  
+                          ),
+                          StreamBuilder<bool>(
+                            stream: playerController.player.playingStream,
+                            builder: (context, snapshot) {
+                              return IconButton(
+                              onPressed: () {
+                                const model = AudioItem(id: "123", title: "东方红", albumTitle: "新中国", artist: "陕北人民", url: "https://cccimg.com/view.php/5d4086139dcd25bef2522c469512c83f.mp3", artwork: "https://cccimg.com/view.php/7ff3bd13cda0aaae9ad0de8d29411f56.jpeg");
+                                playerController.setPlayItem(model);
+                                playerController.handler?.playOrPause();
+                              },  
+                              icon: Icon(playerController.player.isPlaying ? Icons.pause : Icons.play_arrow, color: Colors.white, size: 36,),
+                              );
+                            },  
+                          ),
+                          ]
+                      ),
+                    ),
+                  ),
+                ],
               ),)
             ],
           ),
         ),
       ),
     );
+  }
+
+  AnimatedOpacity _wave() {
+      return AnimatedOpacity(
+        opacity: playerController.player.isPlaying ? 1 : 0, 
+        duration: Duration(milliseconds: 500), 
+        child: WaveWidget(
+          config: CustomConfig(
+            gradients: [
+              [XMColor.xmOrange, Colors.red],
+              [XMColor.xmGreen, XMColor.xmBlue],
+            ],
+            durations: [35000, 10800],
+            heightPercentages: [0.55, 0.60],
+            gradientBegin: Alignment.bottomLeft,
+            gradientEnd: Alignment.topRight,
+          ),
+          size: Size(double.infinity, 60), 
+          waveAmplitude: 0,
+          )
+        );
   }
 }
